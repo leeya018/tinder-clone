46b19e76f6a87d24a4fbf90786cf28d35b4b55f0
leeya018 <leeyahav018@gmail.com>
2024-03-02 20:19:53 +0200

    need to fix that in reacs

--- src/app/recs/page.tsx
+++ src/app/recs/page.tsx
@@ -4,6 +4,8 @@ import { observer } from "mobx-react-lite"
 import recsStore from "@/mobx/recsStore"
 import BottomNav from "@/components/bottomNav"
 import Image from "next/image"
+import TopNav from "@/components/topNav"
+import { FaCircleInfo } from "react-icons/fa6"
 
 const RecsPage = observer(() => {
   const recs = recsStore.recs
@@ -11,21 +13,36 @@ const RecsPage = observer(() => {
   return (
     <div className="h-screen max- w-screen ">
       {/* rec page */}
-      <div className="w-full h-full">
+      <TopNav />
+
+      <div
+        className="border-2 border-black h-full flex 
+      items-center relative rounded-lg overflow-hidden"
+      >
+        <div className="flex justify-center items-center absolute top-20">
+          <div>__</div>
+          <div>__</div>
+          <div>__</div>
+          <div>__</div>
+          <div>__</div>
+        </div>
         <Image
           alt="profile image"
           width={32}
           height={200}
-          className="absolute top-0 left-0 right-0 bottom-0 "
+          className="absolute h-full w-full object-cover "
           src={images[0]}
         />
         <div
-          className="flex items-center text-black  relative w-full mx-4 text-xl 
-         z-10 bottom-0 left-0 right-0 "
+          className="flex items-center 
+           w-full mx-4 text-xl absolute bottom-20 text-white gap-2 
+"
         >
-          <div className="text-xl font-bold">{"name"}</div>
+          <div className="text-xl font-bold">{name}</div>
           <div className="text-lg font-semibold">{age}</div>
-          <div className="ml-auto">info</div>
+          <div className="ml-auto">
+            <FaCircleInfo size={25} />
+          </div>
         </div>
       </div>
 
---
+++ src/components/topNav/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+import { FaFire } from "react-icons/fa"
+
+export default function TopNav() {
+  return (
+    <div className="absolute top-0 left-0 right-0 z-10 bg-white ">
+      <div className="flex items-center gap-2 p-2">
+        <div>
+          <FaFire size={25} color="red" />
+        </div>
+        <div className="text-2xl font-bold text-red-500">Tinder</div>
+      </div>
+    </div>
+  )
+}
